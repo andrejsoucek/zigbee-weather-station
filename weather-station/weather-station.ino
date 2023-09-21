e249227b115859d90ffed44a4d72a65070b8856e
Andrej Souček <andrej.soucek@betsys.com>
2023-09-22 00:09:37 +0200

    added temperature sensor

--- weather-station/weather-station.ino
+++ weather-station/weather-station.ino
@@ -1,13 +1,15 @@
 #include <BME280I2C.h>
 #include <Wire.h>
 #include <EnvironmentCalculations.h>
+#include <OneWire.h>
+#include <DallasTemperature.h>
 
 #define WIND_SAMPLE_INTERVAL_MS 5000
 #define ENV_SAMPLE_INTERVAL_MS 300000
 #define GUST_INTERVAL_MS 600000
 #define WIND_AVERAGE_INTERVAL_MS 60000
 
-#define ALTITUDE_M 242; // weather station altitude to calculate QNH
+#define ALTITUDE_M 242 // weather station altitude to calculate QNH
 
 float maxWindSpeed = 0;
 float totalWindSpeed = 0;
@@ -18,6 +20,8 @@ unsigned long averageStartTime = 0;
 unsigned long envStartTime = 0;
 
 BME280I2C bme;
+OneWire oneWire(9); // temperature sensor pin - D9
+DallasTemperature sensors(&oneWire);
 
 void setup() {
   // initiate serial communication with 9600 baud rate
@@ -32,6 +36,8 @@ void setup() {
     delay(1000);
   }
 
+  sensors.begin();
+
   windStartTime = millis();
   gustStartTime = millis();
   averageStartTime = millis();
@@ -39,6 +45,7 @@ void setup() {
 }
 
 double readWindSpeed() {
+  int analogValue = analogRead(A0); // BME sensor pin - A0
   double voltage = map(analogValue, 0, 1023, 0, 2500); // map 1024 values to 25 V with 2 decimals precision
   double mps = map(voltage, 0, 500, 0, 3000); // map 500 values (5V) to 30 m/s with 2 decimals precision
   mps /= 100; // divide by 100 to get the m/s with 2 decimals
@@ -51,13 +58,14 @@ void loop() {
 
   // read temperature, humidity and pressure
   if (currentTime - envStartTime >= ENV_SAMPLE_INTERVAL_MS) {
-    float tempC(NAN), hum(NAN), presHPa(NAN);
+    float tempSensorC(NAN), hum(NAN), presHPa(NAN);
+    bme.read(presHPa, tempSensorC, hum);
+    float qnh = EnvironmentCalculations::EquivalentSeaLevelPressure(ALTITUDE_M, tempSensorC, presHPa);
 
-    bme.read(presHPa, tempC, hum);
+    sensors.requestTemperatures();
+    float tempC = sensors.getTempCByIndex(0);
 
-    float qnh = EnvironmentCalculations::EquivalentSeaLevelPressure(ALTITUDE_M, tempC, presHPa);
-
-    Serial.print("{\"3\":" + String(tempC, 2) + ",\"4\":" + hum + ",\"5\":" + qnh + "}");
+    Serial.print("{\"3\":" + String(tempC, 2) + ",\"4\":" + hum + ",\"5\":" + qnh + ",\"6\":" + tempSensorC + "}");
 
     envStartTime = currentTime;
   }
